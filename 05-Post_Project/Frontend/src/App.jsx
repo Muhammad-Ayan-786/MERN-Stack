777c1f7029c6a03f7a132ff4f26af685894f3347
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2026-02-28 17:50:10 +0200

    Implement Backend of Post Project

---
+++ 05-Post_Project/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Router, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className='bg-gray-950 w-full h-screen'>
+      <h1 className='text-5xl pt-20 italic underline text-white text-center'>
+        Creating a Post Application with BackEnd & FrontEnd
+      </h1>
+    </div>
+  )
+}
+
+export default App
